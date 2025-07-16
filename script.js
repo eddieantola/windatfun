d0223a3ab832fe4a821010fdb678ef0556df147f
edoardoantola <e.antola@majorbit.com>
2025-07-16 18:40:11 +0200

    miglioramento fade in delle sezioni

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }, {
-        threshold: 0.1
+        threshold: 0.5
     });
 
     animatedElements.forEach(el => {
